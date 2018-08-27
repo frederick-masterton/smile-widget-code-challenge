a7c2b7dd60c8bd4a323912e51bb2ab92d1c1338b
frederick-masterton <frederick.masterton@hotmail.com>
2018-08-27 13:47:26 -0400

    Add ProductPrice model, move price Product model to new ProductPrice model.

--- smilewidgets/products/models.py
+++ smilewidgets/products/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=25, help_text='Customer facing name of product')
-    code = models.CharField(max_length=10, help_text='Internal facing reference to product')
-    price = models.PositiveIntegerField(help_text='Price of product in cents')
-    
+    code = models.CharField(max_length=10, help_text='Internal facing reference to product', )
+
     def __str__(self):
         return '{} - {}'.format(self.name, self.code)
+
+
+class ProductPrice(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE,)
+    price = models.PositiveIntegerField(help_text='Price of product in cents')
+
+    def __str__(self):
+        return '{}'.format(self.price)
 
 
 class GiftCard(models.Model):
